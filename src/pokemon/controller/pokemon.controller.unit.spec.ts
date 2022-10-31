f17f24874c8a1fba19ea211c8372117e6f36ea73
leoronchini <leo.ronchini@hotmail.com>
2022-10-31 17:46:44 -0300

    include ignore

--- src/pokemon/controller/pokemon.controller.unit.spec.ts
+++ src/pokemon/controller/pokemon.controller.unit.spec.ts
@@ -1,20 +1,37 @@
-// import { mock } from 'jest-mock-extended';
+import { mock } from 'jest-mock-extended';
 
-// import { PokemonService } from "../service/pokemon.service";
-// import { PokemonController } from "./pokemon.controller";
+import { PokemonService } from "../service/pokemon.service";
+import { PokemonDto } from './dto/pokemon.dto';
+import { PokemonController } from "./pokemon.controller";
 
-// describe('PokemonController', () => {
-//     const pokemonController = mock<PokemonController>();
+describe('PokemonController', () => {
+    // const pokemonService = mock<PokemonService>();
+    let pokemonService: PokemonService;
+    // const pokemonController = new PokemonController(
+    //     pokemonService,
+    // )
+    const pokemonController =  mock<PokemonController>();
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+
+
+    describe('getPokemon', () => {
+        it('should return a list of pokemon ', () => {
+
+            // const mockPokemon = new PokemonEntity('pokeLink', 'pokeName');
+            // pokemonRepositoryMock.findByName.mockResolvedValue(mockPokemon);
+            // const result = await pokemonService.getPokemon('name');
+
+            // expect(pokemonController).toBeDefined();
+
+            const result = new Promise<PokemonDto>(() => {
+                return new PokemonDto('pokeLink', 'pokeName');
+            });
     
-//     beforeEach(() => {
-//         jest.resetAllMocks();
-//     });
-
-//     // const pokemonService = PokemonService;
-
-//     describe('getPokemon', () => {
-//         it('should return a list of pokemon ', () => {
-            
-//         });
-//     });
-// });
+            // jest.spyOn(pokemonService, 'getPokemonByName').mockImplementation(() => result);
+    
+            expect(pokemonController.getPokemon('name')).toBe(result);
+        });
+    });
+});
